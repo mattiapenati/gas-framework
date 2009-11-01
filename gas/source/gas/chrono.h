7feb9886ac03eb49b94ab3b2ba8c7cd88edccf2d
mattia.penati <mattia.penati@f5836fa8-9f95-11dd-a874-8d3a81574e67>
2009-11-01 20:48:55 +0000

    Basic geometric entities rewritten and documentated

---
+++ gas/source/gas/chrono.h
@@ -0,0 +1,98 @@
+/*
+ * Copyright (c) 2008, Politecnico di Milano
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the Politecnico di Milano nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/*!
+ * @file chrono.h
+ * @brief A chronometer to profile your code
+ */
+
+#ifndef _gas_chrono_
+#define _gas_chrono_
+
+#include <ctime>
+#include <iostream>
+
+namespace gas {
+
+/*! @brief A chronometer to profile your applications */
+class chrono {
+
+public:
+	/*! @brief Create a new chronometer */
+	inline chrono (): time_start_(0), time_elapsed_(0) {
+	}
+
+	/*! @brief Start the chronometer */
+	inline chrono & start () {
+		time_elapsed_ = 0;
+		time_start_ = std::clock();
+		return * this;
+	}
+
+	/*! @brief Stop the chronometer */
+	inline chrono & stop () {
+		time_elapsed_ += (std::clock()-time_start_);
+		return *this;
+	}
+
+	/*! @brief Pause the chronometer */
+	inline chrono & pause () {
+		return stop();
+	}
+
+	/*! @brief Restart the chronometer */
+	inline chrono & restart () {
+		time_start_ = std::clock();
+		return *this;
+	}
+
+	/*! @brief Give the elapsed time */
+	inline std::clock_t elapsed () const {
+		return time_elapsed_;
+	}
+
+	/*! @brief Give the elapsed time in seconds  */
+	inline double elapsed_s () const {
+		return time_elapsed_ / CLOCKS_PER_SEC;
+	}
+
+private:
+	std::clock_t time_start_;
+	std::clock_t time_elapsed_;
+
+};
+
+/*! @brief The overloading of operator<< to print a chrono objec */
+inline std::ostream & operator<< (std::ostream & out, chrono const & c) {
+	return (out << c.elapsed_s() << "s");
+}
+
+}
+
+#endif // _gas_chrono_
---
+++ gas/source/geometry/unit/square.h
@@ -0,0 +1,68 @@
+/*
+ * Copyright (c) 2008, Politecnico di Milano
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the Politecnico di Milano nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/*!
+ * @file square.h
+ * @brief The square obtained by cartesian product of two intervals,
+ *        \f$(-1,1)\times(-1,1)\f$
+ */
+
+#ifndef _gas_geometry_unit_square_
+#define _gas_geometry_unit_square_
+
+#include "interval.h"
+
+namespace gas { namespace geometry { namespace unit {
+
+/*!
+ * @brief The square obtained by cartesian product of two intervals,
+ *        \f$(-1,1)\times(-1,1)\f$
+ */
+class square {
+
+public:
+	/*! @brief The self type */
+	typedef square self_t;
+
+public:
+	/*!
+	 * @brief Check the membership of a point by its coordinates
+	 * @param x The first coordinate of point
+	 * @param y The second coordinate of point
+	 * @return True if the points is locate in the square
+	 */
+	static inline bool in (double const & x, double const & y) {
+		return (interval::in(x) and interval::in(y));
+	}
+
+};
+
+} } }
+
+#endif // _gas_geometry_unit_square_
